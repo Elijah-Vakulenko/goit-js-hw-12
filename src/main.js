e480a6a753bd499e7a926a4fccbf2fc4511e4dd1
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-05-27 21:28:59 +0100

    main function edit
    Image loader function adapted

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 import axios from 'axios';
 
-
 import { getImages } from './js/pixabay-api.js'; //← функція фетчу зображень з серверу
 import { galleryRender} from './js/render-functions.js'; //← імпорт функції рендеру
 
@@ -17,10 +16,10 @@ const refs = {
     form: document.querySelector('.search-form'),
     gallery: document.querySelector('.gallery'),
     loader: document.querySelector('.loader'),
-    loadMoreBtn: document.querySelector('.load'),
+    loadMoreBtn: document.querySelector('.load-more'),
 }
 
-let currentPage = 1; //← відображаємо номер поточної сторінки (після кожних 20 фото завантажуємо наступні 20, тобто додаємо нову сторінку)
+let currentPage = 1; //← відображаємо номер поточної сторінки (після кожних 15 фото завантажуємо наступні 15, тобто додаємо нову сторінку)
 
 // //===============================================================================
 // // Налаштовуємо SimpleLightbox
@@ -68,64 +67,56 @@ function getBoundingClientRect() {
 // //===============================================================================
 // // Налаштовуємо слухач подій та визначаємо, що буде відбуватися при запиті
 
-// refs.form.addEventListener('submit', event => {
-//     event.preventDefault();
-//     const query = event.target.elements.query.value.trim();
-//     currentPage = 1;
+refs.form.addEventListener('submit', event => {
+    event.preventDefault();
+    const query = event.target.elements.query.value.trim();
+    currentPage = 1;
 
-//     refs.loader.classList.remove('is-hidden'); //← видаляємо клас visually hidden щоб показати, що йде пошук, сторінка чекає на завантаження даних.
+    refs.loader.classList.remove('is-hidden'); //← видаляємо клас visually hidden щоб показати, що йде пошук, сторінка чекає на завантаження даних.
     
-//     getImages(query).then(data => {
-//            if (data.hits.length === 0) {
-//             // якщо немає результатів пошуку:
-//                if (refs.loadMoreBtn) {
-//                     refs.loadMoreBtn.remove(); // ← видаляю кнопку load more
-//                 }
-//                showNoResultsMessage(); //← запускаємо функцію оповіщення від iziToast;
-//                refs.gallery.innerHTML = '';//← очищуємо галерею, якщо там лишився результат минулого пошуку;
-//         } else {
-//             //Якщо пошук за ключем дає результат, запускаємо рендер та виводимо зображення за допомогою Lightbox
-//             const markup = galleryRender(data.hits);
-//             refs.gallery.innerHTML = markup;
-//                initilizeLightbox(); //← підключаємо Лайтбокс
+    getImages(query).then(data => {
+           if (data.hits.length === 0) {
+            // якщо немає результатів пошуку:
+               if (refs.loadMoreBtn) {
+                    refs.loadMoreBtn.remove(); // ← видаляю кнопку load more
+                }
+               showNoResultsMessage(); //← запускаємо функцію оповіщення від iziToast;
+               refs.gallery.innerHTML = '';//← очищуємо галерею, якщо там лишився результат минулого пошуку;
+        } else {
+            //Якщо пошук за ключем дає результат, запускаємо рендер та виводимо зображення за допомогою Lightbox
+            const markup = galleryRender(data.hits);
+            refs.gallery.innerHTML = markup;
+               initilizeLightbox(); //← підключаємо Лайтбокс
 
-//                //Тепер, після того як в нас вже завантажилися 20 зображень, ми створюємо кнопку "Завантажити ще"
+               //Тепер, після того як в нас вже завантажилися 20 зображень, ми створюємо кнопку "Завантажити ще"
 
 
-//                if (data.totalHits > 20) {
-//                 if (refs.loadMoreBtn) { //← якщо кнопка завантажити ще лишилася з минулого запиту то видаляємо її
-//                     refs.loadMoreBtn.remove(); 
-//                 }
-//                 refs.loadMoreBtn = document.createElement('button');
-//                 refs.loadMoreBtn.textContent = 'Load More';
-//                 refs.loadMoreBtn.classList.add('load-more');
-//                 refs.gallery.insertAdjacentElement('afterend', refs.loadMoreBtn); // ← додаємо кнопку після галереї
-//                 refs.loadMoreBtn.addEventListener('click', loadMoreImages); // ← Викликаємо функцію завантаження наступних 20 зображень
-//             }
-//         }
-//         // Ховаємо індикатор завантаження даних після завантаження зображень
-//         refs.loader.classList.add('is-hidden');
-//     });
-// });
+               if (data.totalHits > 15) {
+                refs.loadMoreBtn.classList.remove('is-hidden');
+                refs.loadMoreBtn.addEventListener('click', loadMoreImages); // ← Викликаємо функцію завантаження наступних 20 зображень
+            }
+        }
+        // Ховаємо індикатор завантаження даних після завантаження зображень
+        refs.loader.classList.add('is-hidden');
+    });
+});
 
-// //============================================================================
-// // Додаємо наступну сторінку з 15ма зображеннями при кліку по кнопці
+//============================================================================
+// Додаємо наступну сторінку з 15ма зображеннями при кліку по кнопці
 
-// function loadMoreImages() {
-//     currentPage++; // 
-//     const query = refs.form.elements.query.value.trim();
+function loadMoreImages() {
+    currentPage++; 
+    const query = refs.form.elements.query.value.trim();
 
-//     refs.loader.classList.remove('is-hidden');
+    refs.loader.classList.remove('is-hidden');
 
-//     getImages(query, currentPage).then(data => {
-//         if (data.hits.length === 0) {
-//             showNoResultsMessage();
-//         } else {
-//             const markup = galleryRender(data.hits);
-//             refs.gallery.insertAdjacentHTML('beforeend', markup);
-
-//        
-
-//         refs.loader.classList.add('is-hidden');
-//     });
-// }
+    getImages(query, currentPage).then(data => {
+        if (data.hits.length === 0) {
+            showNoResultsMessage();
+        } else {
+            const markup = galleryRender(data.hits);
+            refs.gallery.insertAdjacentHTML('beforeend', markup);
+            refs.loader.classList.add('is-hidden');
+        };
+    });
+};
